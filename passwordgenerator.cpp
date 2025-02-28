ccc5d6f94af27543027e683cdf1c243323295641
Darwin Zmugg <117575492+666hwll@users.noreply.github.com>
2025-02-28 19:36:37 +0100

    converted the c code to c++ (will improve)

--- passwordgenerator.cpp
+++ passwordgenerator.cpp
@@ -1,7 +1,11 @@
-#include <stdio.h>
+//#include <stdio.h>
+#include <iostream>
+#include <string>
+
 #include <stdlib.h>
 #include <sys/random.h>
-#include <string.h>
+//#include <string.h>
+
 
 // struct
 unsigned int choice;
@@ -10,31 +14,37 @@ int secrets (size_t x) {
     do {
         getrandom(&choice, sizeof(choice),  0);
         choice = choice %  x;
-    } while(choice < 0); 
-   
+    } while(choice < 0);
+
    return 0;
 }
 
 int main() {
     unsigned int leni;
-    char end[4];
-    const char asym[] = "^1jkl˝^<2ASDFGHJKL3{[]}qwertzu4P5)=?`iop6IO+@~ a7ERTZUsdfg8*QWhyx9cvbnm,0°!§$&/(.-<YXCVBNM;:_|" ;
+    std::string end = "";
+    const std::string asym = "^1jkl˝^<2ASDFGHJKL3{[]}qwertzu4P5)
+=?iop6IO+@~ a7ERTZUsdfg8*QWhyx9cvbnm,0°!§$&/(.-<YXCVBNM;:_|" ;
     const size_t n = sizeof(asym)/sizeof(asym[0]);
-    printf("Type in the length of the password you want to be generated(X to quit): ");
-    if (scanf_s("%d", &leni) != 1) {
-        printf("Input isn't expected format.\n");
-        exit(1);
-    }
-   char strb[leni + 1];
-   
+    std::cout << "Type in the length of the password you want to
+ be generated(X to quit): ";
+    //std::cin >> leni;
+    if (!(std::cin >> leni)) {
+        std::cerr << "Input isn't expected format.\n";
+        exit(1);          }
+    std::string strb;
+    strb.resize(leni);
+
    for (int len = leni; len > 0; len--) {
        secrets(n);
-       strb[len-1] = asym[choice]; // Use choice as an index to access the asym array
+       strb[len-1] = asym[choice]; // Use choice as an index to
+access the asym array
    }
-   printf("%s\n", strb);
+   std::cout << strb << "\n";
+   //printf("%s\n", strb);
    do {
-   scanf("\n%c", &end);
-   } while (strcmp(end, "X"));
-   
+        std::cin >> end;
+   //scanf("\n%c", &end);
+   } while ("X" != end);
+
    return 0;
-}
+}             
